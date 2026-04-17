b4579ffb2ae2901c5d9c0e24bdc3d4d3cfaa0d79
YosimotoRyou <mrrhr888@gmail.com>
2026-04-17 13:33:07 +0900

    update: ベース処理完成

--- Assets/Editor/Editor_Base.cs
+++ Assets/Editor/Editor_Base.cs
@@ -7,7 +7,33 @@ using UnityEditor;
  * 履歴　 : 2026/04/17 新規作成(吉本竜)
  ================================*/
 
+/// <summary>
+/// ツール用のEditorWindow作成ベース
+/// </summary>
 public class Editor_Base : EditorWindow
 {
+    /// <summary>
+    /// menuからウィンドウを開く。
+    /// </summary>
+    [MenuItem("Tools/ScriptableObjects/Test")]
+    public static void ShowWindow()
+    {
+        GetWindow<Editor_Base>("Test");
+    }
 
+    /// <summary>
+    /// ウィンドウが有効になったタイミングで呼ばれる。
+    /// </summary>
+    private void OnEnable()
+    {
+        // 必要になったら初期化
+    }
+
+    /// <summary>
+    /// GUIを描画する（IMGUI）。
+    /// </summary>
+    private void OnGUI()
+    {
+
+    }
 }
