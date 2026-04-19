63d8da4d6e41dfbb125805ddcea07a14192b615f
UrunoRikuto <nagito2314@gmail.com>
2026-04-19 11:10:23 +0900

    Update: CreateThiefSceneの編集

--- Assets/Programmer/Scripts/Wave/WaveManager.cs
+++ Assets/Programmer/Scripts/Wave/WaveManager.cs
@@ -10,9 +10,14 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    [Tooltip("ウェーブ数"), Min(0)]
+    [Tooltip("ウェーブ数"), Min(1)]
     private int waveCount;
     public int waveNumber => waveCount;
+
+    private void Start()
+    {
+        Reset();
+    }
 
     // ウェーブ数をカウントアップする
     public void CountUp()
@@ -23,6 +28,6 @@ public class WaveManager : MonoBehaviour
     // ウェーブ数をリセットする
     public void Reset()
     {
-        waveCount = 0;
+        waveCount = 1;
     }
 }
