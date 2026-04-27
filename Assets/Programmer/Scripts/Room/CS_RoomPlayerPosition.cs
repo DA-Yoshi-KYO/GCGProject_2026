960064a4a51e94399ac3cd332df97c1a2cfb11e6
KR <shiguma665hal@gmail.com>
2026-04-28 01:55:08 +0900

    update: planeを単体でも取得できるように更新

--- Assets/Programmer/Scripts/Room/CS_RoomPlayerPosition.cs
+++ Assets/Programmer/Scripts/Room/CS_RoomPlayerPosition.cs
@@ -5,6 +5,7 @@ using UnityEngine;
  *  制作者      : 吉本竜
  *  内容        : Playerが現在いるRoomCreatePointを管理するクラス
  *  履歴        : 2026/04/27 新規作成(ヨシモト)
+ *                2026/04/28 CreatePoint更新時にplaneObjectも探索するように更新(ヨシダ)
  *==================================================*/
 
 /// <summary>
@@ -17,6 +18,7 @@ public class CS_RoomPlayerPosition : MonoBehaviour
     private GameObject player;
 
     private GameObject playerRoomData;
+    [HideInInspector] public GameObject planeObject { private set; get; }
 
     /// <summary>
     /// RaycastでPlayerが現在いるRoomCreatePointを更新します。
@@ -24,6 +26,19 @@ public class CS_RoomPlayerPosition : MonoBehaviour
     public void RefreshPlayerRoomData()
     {
         playerRoomData = CS_RoomCreatePointRaycast.GetRayRoomCreatePoint(player);
+
+        if (playerRoomData != null)
+        {
+            Transform[] children = playerRoomData.GetComponentsInChildren<Transform>();
+            foreach (Transform t in children)
+            {
+                if (t.name == "Plane")
+                {
+                    planeObject = t.gameObject;
+                    break;
+                }
+            }
+        }
     }
 
     /// <summary>
