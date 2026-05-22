fbf136933a5286efb0a000799a11958144a046c2
AT12A_YOSHIDA <shiguma665hal@gmail.com>
2026-05-22 13:00:21 +0900

    feat: Playerマテリアル用CSの作成

---
+++ Assets/Programmer/Scripts/Player/CS_PlayerMaterial.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CS_PlayerMaterial : MonoBehaviour
+{
+    [SerializeField] private GameObject playerMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
