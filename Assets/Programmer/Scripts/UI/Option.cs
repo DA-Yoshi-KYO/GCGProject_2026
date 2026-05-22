5b3c492499c52bbbdd783a0d1fc06d66ad92b3d5
SIRONAGA <akinosyoe@gmail.com>
2026-05-22 12:16:37 +0900

    fix: タイトル画面で開かなくなるように変更

--- Assets/Programmer/Scripts/UI/Option.cs
+++ Assets/Programmer/Scripts/UI/Option.cs
@@ -1,5 +1,7 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 // 制作者　秋野
 
 
@@ -72,6 +74,8 @@ public class Option : MonoBehaviour
     /// </summary>
     private void OnOption(InputAction.CallbackContext ctx)
     {
+        // タイトルシーンではオプションUIを開かない
+        if (SceneManager.GetActiveScene().name.ToString() == "Title") return;
         if (!_isOptionUIActive)
         {
             OpenOptionUI();
