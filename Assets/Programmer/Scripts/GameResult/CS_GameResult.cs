15dfe35389219e119b52041f96f93c3c10c1d654
Ruan-3144 <esapamimishi@gmail.com>
2026-06-04 11:17:25 +0900

    feat: リザルトの作成
    UIのボタンの表示がバグっているので、修正必要です

---
+++ Assets/Programmer/Scripts/GameResult/CS_GameResult.cs
@@ -0,0 +1,68 @@
+/* ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+ *    ゲームリザルトの作成
+ * ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+ *    元浪梨緒
+ * ----------------------------------------------------------
+ * 2026-06-04 | 初回作成
+ */
+using UnityEngine;
+
+public class CS_GameResult : MonoBehaviour
+{
+    [Header("ゲームクリアのPrefab格納")][SerializeField] private GameObject gameClear;
+    [Header("ゲーム失敗のPrefab格納")][SerializeField] private GameObject gameFailure;
+
+    private CS_EndManager endManager;
+    bool end = false;
+
+    [Header("デバッグの確認時用")][SerializeField] public bool debugEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        endManager = GameObject.Find("EndManager").GetComponent<CS_EndManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (debugEnd)
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                end = true;
+            }
+
+            if (end)
+            {
+                gameClear.SetActive(true);
+            }
+            else
+            {
+                gameClear.SetActive(false);
+                gameFailure.SetActive(false);
+            }
+        }
+        else
+        {
+            if (endManager.read_IsEnd)
+            {
+                if (endManager.read_IsWin)
+                {
+                    gameClear.SetActive(true);
+                }
+                else
+                {
+                    gameFailure.SetActive(true);
+                }
+            }
+            else
+            {
+                gameClear.SetActive(false);
+                gameFailure.SetActive(false);
+            }
+        }
+    }
+}
