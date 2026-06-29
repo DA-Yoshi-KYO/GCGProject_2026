19e66af93e16ae3274b0ab57178e01bfb0824edf
P1GUM1 <akinosyoe@gmail.com>
2026-06-29 13:05:45 +0900

    fix: 分身ネコタンの修正

--- Assets/Programmer/Scripts/Gimmick/CloneCat.cs
+++ Assets/Programmer/Scripts/Gimmick/CloneCat.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CloneCat : GimmickBase
 {
+    [Header("効果時間")]
     [SerializeField] private float ActiveTime = 20.0f;//秒数
+    [SerializeField] private GameObject CloneCatActor;
 
     GameObject player;
+    GameObject cloneCatActor;
     CS_RoomPlayerPosition roomPlayerPosition;
     float activeTimer = 0.0f;
     bool bFirstBroken = true;
@@ -15,28 +18,39 @@ public class CloneCat : GimmickBase
     {
         base.IdleUpdate();
         gimmickState = GimmickState.Active;
-        player = GameObject.Find("Player(Clone)");
+        player = GameObject.Find("Player(Clone)"); 
+
         if(player != null)
         {
             Debug.Log("CloneCat: Player found");
         }
         roomPlayerPosition = GameObject.Find("RoomManager").GetComponent<CS_RoomPlayerPosition>();
 
+        if (player == null) return;
+        CS_PlayerMove move = player.GetComponent<CS_PlayerMove>();
+
+        if (move == null) return;
+        move.SetInvincibleFlag(true);
+
+        CS_PlayerAction action = player.GetComponent<CS_PlayerAction>();
+        if (action == null) return;
+        action.SetViewPreview(false);
+        action.SetSelectGimmickActive(false);
+
+
+        cloneCatActor = GameObject.Find("CloneCatActor(Clone)");
+        if(cloneCatActor != null)
+        {
+            player.transform.position = cloneCatActor.transform.position;
+            ChengePosProcess();
+        }
+
+        ViewGimmickUI(false);
+
     }
 
     protected override void ActiveUpdate()
     {
-        base.ActiveUpdate();
-        if (player == null) return;
-        CS_PlayerMove move = player.GetComponent<CS_PlayerMove>();
-        if (move == null) return;
-        move.SetInvincibleFlag(true);
-        CS_PlayerAction action = player.GetComponent<CS_PlayerAction>();
-        if (action == null) return;
-        action.SetViewPreview(false);
-
-
-
         // アクティブ状態の時間をカウント
         activeTimer += Time.deltaTime;
         if(activeTimer >= ActiveTime)
@@ -44,21 +58,30 @@ public class CloneCat : GimmickBase
             gimmickState = GimmickState.Broken;
         }
     }
-
+    
     protected override void BrokenUpdate()
     {
         base.BrokenUpdate();
 
         if (!bFirstBroken) return;
         bFirstBroken = false;
+
+        ViewGimmickUI(true);
+
         if (player != null)
         {
+            cloneCatActor = GameObject.Find("CloneCatActor(Clone)");
+            if(cloneCatActor == null)
+            {
+                cloneCatActor = Instantiate(CloneCatActor, this.transform.position, Quaternion.identity);
+            }
+            cloneCatActor.transform.position = player.transform.position;
+
+
             Vector3 WarpPosition = this.transform.position;
             WarpPosition.y = 5.0f;
             player.transform.position = WarpPosition;
-            roomPlayerPosition.RefreshPlayerRoomData();
-            CS_PlayerData playerdata = player.GetComponent<CS_PlayerData>();
-            playerdata.ChangePlayerRoomData();
+            ChengePosProcess();
 
             CS_PlayerMove move = player.GetComponent<CS_PlayerMove>();
             if (move != null)
@@ -69,11 +92,61 @@ public class CloneCat : GimmickBase
             if (action != null)
             {
                 action.SetViewPreview(true);
+                action.SetSelectGimmickActive(true);
             }
-            CS_PlayerCamera camera = player.GetComponent<CS_PlayerCamera>();
-            if (camera == null) return;
-            camera.ChangeCamera();
         }
     }
 
+    void ChengePosProcess()
+    {
+        roomPlayerPosition.RefreshPlayerRoomData();
+        if (player != null)
+        {
+            CS_PlayerData playerdata = player.GetComponent<CS_PlayerData>();
+            if (playerdata != null)
+            {
+                playerdata.ChangePlayerRoomData();
+            }
+            CS_PlayerCamera camera = player.GetComponent<CS_PlayerCamera>();
+            if (camera != null)
+            {
+                camera.ChangeCamera();
+            }
+
+        }
+    }
+
+    void ViewGimmickUI(bool bView)
+    {
+        GameObject Canvase = GameObject.Find("Canvases");
+        if (Canvase == null)
+        {
+            Debug.Log("Canvase not found");
+            return;
+        }
+        GameObject　gimmickUI = GameObject.Find("GameUICavas");
+        if (gimmickUI == null)
+        {
+            Debug.Log("gimmickUI not found");
+            return;
+        }
+        GameObject gimmikF = gimmickUI.transform.Find("GimmickF").gameObject;
+        if(gimmikF != null)gimmikF.SetActive(bView);
+        GameObject ctMask1 = gimmickUI.transform.Find("CTMask1").gameObject;
+        if(ctMask1 != null)ctMask1.SetActive(bView);
+        GameObject ctMask2 = gimmickUI.transform.Find("CTMask2").gameObject;
+        if(ctMask2 != null)ctMask2.SetActive(bView);
+        GameObject gimmick = gimmickUI.transform.Find("Gimmick").gameObject;
+        if(gimmick != null)gimmick.SetActive(bView);
+        GameObject gimmickSelectUI = gimmickUI.transform.Find("GimmickSelectUI").gameObject;
+        if (gimmickSelectUI != null) gimmickSelectUI.SetActive(bView);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gimmickState = GimmickState.Broken;
+        }
+    }
 }
