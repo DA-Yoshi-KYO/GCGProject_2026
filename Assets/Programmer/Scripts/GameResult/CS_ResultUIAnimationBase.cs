f96c215df82cac83b953b09d69bd20c3131a8b22
AT12A_YOSHIDA <shiguma665hal@gmail.com>
2026-07-03 09:51:11 +0900

    feat: Canvasの追加

--- Assets/Programmer/Scripts/GameResult/CS_ResultUIAnimationBase.cs
+++ Assets/Programmer/Scripts/GameResult/CS_ResultUIAnimationBase.cs
@@ -53,16 +53,89 @@ public class CS_ResultUIAnimationBase : MonoBehaviour
     // 経過時間計測
     protected float timer = 0.0f;
 
+    [Header("外部から格納するUI情報"), SerializeField]
+    protected TransitionData[] inputDatas =
+    {
+        new TransitionData()
+        {
+            transitionPos = new Vector3Data() { init = new Vector3(0f, 0f, 0f), target = new Vector3(0f, 0f, 0f) },
+            transitionScale = new Vector3Data() { init = new Vector3(1f, 1f, 1f), target = new Vector3(1f, 1f, 1f) },
+            transitionRotate = new Vector3Data() { init = new Vector3(0f, 0f, 0f), target = new Vector3(0f, 0f, 0f) },
+            transitionColor = new ColorData() { init = new Color(1f, 1f, 1f, 1f), target = new Color(1f, 1f, 1f, 1f) },
+
+            useInitPos = true,
+            useInitScale = true,
+            useInitRotate = true,
+            useInitColor = true,
+
+            posEaseKind = Easing.EaseKind.Linear,
+            scaleEaseKind = Easing.EaseKind.Linear,
+            rotateEaseKind = Easing.EaseKind.Linear,
+            colorEaseKind = Easing.EaseKind.Linear,
+
+            imageObject = null
+        }
+    };
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        if (phase.Length <= progressIndex)
+            return;
+
+        float duration = phase[progressIndex].animationDuration;
+        timer += Time.deltaTime;
+
+        foreach (int index in phase[progressIndex].sameTimeAnimationIndex)
+        {
+            TransitionData data = inputDatas[index];
+            GameObject imageObject = data.imageObject;
+            RectTransform rectTransform = imageObject.GetComponent<RectTransform>();
+            if (!data.useInitPos)
+            {
+                rectTransform.anchoredPosition = data.transitionPos.init +
+                (data.transitionPos.target - data.transitionPos.init) *
+                Easing.Ease(data.posEaseKind, timer, duration);
+            }
+            if (!data.useInitScale)
+            {
+                rectTransform.localScale = data.transitionScale.init +
+                (data.transitionScale.target - data.transitionScale.init) *
+                Easing.Ease(data.scaleEaseKind, timer, duration);
+            }
+            if (!data.useInitRotate)
+            {
+                Vector3 initRotateRadian = new Vector3(
+                    data.transitionRotate.init.x * Mathf.Deg2Rad,
+                    data.transitionRotate.init.y * Mathf.Deg2Rad,
+                    data.transitionRotate.init.z * Mathf.Deg2Rad);
+                Vector3 targetRotateRadian = new Vector3(
+                    data.transitionRotate.target.x * Mathf.Deg2Rad,
+                    data.transitionRotate.target.y * Mathf.Deg2Rad,
+                    data.transitionRotate.target.z * Mathf.Deg2Rad);
+                rectTransform.Rotate(initRotateRadian +
+                (targetRotateRadian - initRotateRadian) *
+                Easing.Ease(data.rotateEaseKind, timer, duration));
+            }
+            if (!data.useInitColor)
+            {
+                UnityEngine.UI.Image image = imageObject.GetComponent<UnityEngine.UI.Image>();
+                image.color = data.transitionColor.init +
+                (data.transitionColor.target - data.transitionColor.init) *
+                Easing.Ease(data.colorEaseKind, timer, duration);
+            }
+        }
+
+        if (timer > duration)
+        {
+            progressIndex++;
+            timer = 0f;
+        }
     }
 }
