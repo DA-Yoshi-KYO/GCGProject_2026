4f3b97dcc64d373b4f5c05db6cb75b48ea0284c1
YosimotoRyou <mrrhr888@gmail.com>
2026-07-06 01:01:34 +0900

    feat: アンク実装

--- Assets/Programmer/Scripts/Gimmick/MagicAnkhGimmick.cs
+++ Assets/Programmer/Scripts/Gimmick/MagicAnkhGimmick.cs
@@ -3,32 +3,97 @@ using UnityEngine;
 public class MagicAnkhGimmick : GimmickBase
 {
     [SerializeField]
-    private float activeTime = 0.5f;
+    private float activeTime = 1.5f;
 
-    bool isBrokenFirst = false;
+    [Header("アンク発動時Effect")]
+    [SerializeField]
+    private GameObject go_AnkhEffectPrefab;
+
+    [Header("Effect位置Offset")]
+    [SerializeField]
+    private Vector3 v3_EffectOffset = Vector3.zero;
+
+    private bool isActiveFirst = false;
+    private bool isBrokenFirst = false;
+
+    private CSAD_EffectCommonProcessBase csad_AnkhEffect;
 
     protected override void IdleUpdate()
     {
     }
+
     protected override void ActiveUpdate()
     {
-        //プレイヤーアクションで
-        //アンク指定時に鳴き声発生でアクティブ
+        if (isActiveFirst == false)
+        {
+            isActiveFirst = true;
+            PlayAnkhEffect();
+        }
+
         SetHitChecker(transform.position);
-        
-        if(activeTime <= 0)
+
+        if (activeTime <= 0)
         {
             gimmickState = GimmickState.Broken;
         }
 
         activeTime -= Time.deltaTime;
     }
+
     protected override void BrokenUpdate()
     {
         base.BrokenUpdate();
 
         if (isBrokenFirst) return;
-            isBrokenFirst = true;
-            DeleteHitChecker();
+
+        isBrokenFirst = true;
+
+        DeleteHitChecker();
+
+        EndAnkhEffect();
+    }
+
+    private void PlayAnkhEffect()
+    {
+        if (go_AnkhEffectPrefab == null)
+        {
+            return;
+        }
+
+        Vector3 v3_EffectPosition =
+            transform.position + v3_EffectOffset;
+
+        Quaternion q_EffectRotation =
+            go_AnkhEffectPrefab.transform.rotation;
+
+        csad_AnkhEffect = CS_EffectFactory.CreateEffect(
+            go_AnkhEffectPrefab,
+            v3_EffectPosition,
+            q_EffectRotation,
+            null);
+
+        if (csad_AnkhEffect == null)
+        {
+            return;
+        }
+
+        CSST_EffectPlayData csst_EffectPlayData = new CSST_EffectPlayData();
+        csst_EffectPlayData.CSST_EffectPlayData_Init();
+
+        csst_EffectPlayData.SetPosition(v3_EffectPosition);
+        csst_EffectPlayData.SetRotation(q_EffectRotation);
+
+        csad_AnkhEffect.PlayEffect(csst_EffectPlayData);
+    }
+
+    private void EndAnkhEffect()
+    {
+        if (csad_AnkhEffect == null)
+        {
+            return;
+        }
+
+        csad_AnkhEffect.EndEffect();
+        csad_AnkhEffect = null;
     }
 }
