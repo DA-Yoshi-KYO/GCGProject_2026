4e32a221b36dcf83ab3acfb562f4cddb590f08b7
P1GUM1 <akinosyoe@gmail.com>
2026-07-06 11:52:31 +0900

    fix: お宝アイコンが距離取得できるように変更

--- Assets/Programmer/Scripts/UI/TreasureItem.cs
+++ Assets/Programmer/Scripts/UI/TreasureItem.cs
@@ -8,44 +8,80 @@ public class TreasureItem : MonoBehaviour
     [SerializeField] private GameObject DetailTextImage;
     [SerializeField] private GameObject DistanceImage;
     [SerializeField] private GameObject NumberView;
+    [SerializeField] private GameObject FrameIcon;
 
     [Header("スプライト")]
     [SerializeField] private Sprite Moveing;
     [SerializeField] private Sprite Pinch;
 
+    CS_VisionTarget visionTarget;
+    NumberView numberView;
     int Distance = 100;
 
     private void Awake()
     {
         BastetOutLineIcon.SetActive(false);
         DetailTextImage.SetActive(false);
+        numberView = NumberView.GetComponent<NumberView>();
     }
 
     private void FixedUpdate()
     {
-     
+        if (visionTarget == null)
+        {
+            return;
+        }
+        else
+        {
+            BastetIcon.SetActive(false);
+            BastetOutLineIcon.SetActive(false);
+            DetailTextImage.SetActive(false);
+            DistanceImage.SetActive(false);
+            NumberView.SetActive(false);
+            FrameIcon.SetActive(true);
+        }
+
+        // 宝物が盗まれているかどうかを確認
+
+        if (visionTarget.read_IsStolenMoveing)
+        {
+            SetViewMoveing();
+        }
+        else
+        {
+
+        }
+
+        Distance = visionTarget.read_ExitDistance;
+
+        // 距離表示
+
+        if (numberView != null)
+        {
+            numberView.SetNumber(Distance);
+        }
     }
 
 
-    void ViewSetting(int i)
+    void SetViewMoveing()
     {
-        switch(i)
-        {
-            case 0:
-                {
-                }
-            break;
-            case 1:
-                {
-                }
-            break;
-            case 2:
-                {
+        BastetIcon.SetActive(false);
+        BastetOutLineIcon.SetActive(true);
+        DetailTextImage.SetActive(true);
 
-                }
-            break;
+        if (Distance <= 10)
+        {
+            DistanceImage.GetComponent<UnityEngine.UI.Image>().sprite = Pinch;
         }
-        return;
+        else
+        {
+            DistanceImage.GetComponent<UnityEngine.UI.Image>().sprite = Moveing;
+        }
+    }
+    
+    public void SetCS(CS_VisionTarget target)
+    {
+        visionTarget = target;
     }
 
 }
