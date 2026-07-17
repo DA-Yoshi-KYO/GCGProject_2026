233ecc8706a487b491e537e1cc3e132ff5cedda8
AT12A_YOSHIDA <shiguma665hal@gmail.com>
2026-07-17 12:58:19 +0900

    fix: 泥棒から捕まった際の座標補正を取り消し

--- Assets/Programmer/Scripts/Player/CS_PlayerMove.cs
+++ Assets/Programmer/Scripts/Player/CS_PlayerMove.cs
@@ -370,10 +370,6 @@ public class CS_PlayerMove : MonoBehaviour
     /// </summary>
     public void CaughtByThief(float holdCatTime, Transform thiefTransform)
     {
-        transform.position = new Vector3(thiefTransform.position.x, thiefTransform.position.y - thiefTransform.localScale.y / 2.0f, thiefTransform.position.z);
-        transform.position += thiefTransform.forward;
-        visualModel.position = transform.position;
-
         // フラグを立てる
         catCaughtTime = holdCatTime;
         invincibleTime = holdCatTime * 2f;
